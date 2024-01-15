303f1439ea6e8b517a9faed146b63a592c9ca922
Ayaan <122686385+codyaan@users.noreply.github.com>
2024-01-15 18:40:37 +0530

    Updatedd script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ ScrollReveal().reveal('.home-img, .services-container, ', {origin:'bottom'});
 ScrollReveal().reveal('.home-content h1, .about-img', {origin:'left'});
 ScrollReveal().reveal('.home-content p, .about-content', {origin:'right'});
 const typed = new Typed('.multiple-text', {
-  strings: ['Student', 'Learner','Principled','Thinker','Risk Taker','Programmer'],
+  strings: ['Student', 'Learner','Thinker','Risk Taker','Programmer'],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
